62cbbf8400e0331129bd5fc26a6416a70e79c41f
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-08-22 18:07:52 +0300

    work on navbar

--- src/components/mainNavbar/MainNavbar.js.js
+++ src/components/mainNavbar/MainNavbar.js.js
@@ -1,4 +1,20 @@
+import "./styles.css";
+import logo from "../../images/kırmızıdurkalogo.svg";
+import logoyazi from "../../images/durkayazımavi.svg";
+
 function MainNavbar(params) {
-  return <p>asdasd</p>;
+  return (
+    <div className="maincontainer">
+      <div className="brandcontainer">
+        <a href="https://durkaplastic.com" target="_blank" rel="noreferrer">
+          <div className="durkalogo">
+            <img id="logoimg" src={logo} alt="20likbidon" />
+            <img id="logoyazi" src={logoyazi} alt="30lukbidon" />
+          </div>
+        </a>
+      </div>
+      <div className="menucontainer"></div>
+    </div>
+  );
 }
 export default MainNavbar;
