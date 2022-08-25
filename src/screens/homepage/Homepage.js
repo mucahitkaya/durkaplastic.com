b767567d5a75a1570d0578ecd54a995a7028d8bf
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-08-25 18:03:10 +0300

    footer,products & comm page left

--- src/screens/homepage/Homepage.js
+++ src/screens/homepage/Homepage.js
@@ -1,6 +1,7 @@
 import ImageSection from "../../components/imageSection/ImageSection";
+import mavibidon from "../../images/mavibidon.png";
 import "./homepage.css";
-function HomePage(params) {
+function HomePage() {
   return (
     <>
       <ImageSection />
@@ -10,20 +11,36 @@ function HomePage(params) {
         </div>
         <div className="productlistcontainer">
           <div className="card">
-            <div className="card-image"></div>
-            <div className="card-image"></div>
+            <div className="card-image">
+              <img alt="20lik Bidon " src={mavibidon} />
+            </div>
+            <div className="card-image-title">
+              <h3>Gıda Grubu</h3>
+            </div>
           </div>
           <div className="card">
-            <div className="card-image"></div>
-            <div className="card-image"></div>
+            <div className="card-image">
+              <img alt="30lik Bidon" src={mavibidon} />
+            </div>
+            <div className="card-image-title">
+              <h3>Kimya Grubu</h3>
+            </div>
           </div>
           <div className="card">
-            <div className="card-image"></div>
-            <div className="card-image"></div>
+            <div className="card-image">
+              <img alt="25lik bidon" src={mavibidon} />
+            </div>
+            <div className="card-image-title">
+              <h3>Temizlik Grubu</h3>
+            </div>
           </div>
           <div className="card">
-            <div className="card-image"></div>
-            <div className="card-image"></div>
+            <div className="card-image">
+              <img alt="30luk bidon" src={mavibidon} />
+            </div>
+            <div className="card-image-title">
+              <h3>Tarım Grubu</h3>
+            </div>
           </div>
         </div>
       </div>
